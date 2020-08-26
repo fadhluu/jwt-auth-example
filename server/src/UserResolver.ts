f975256d2fe607c3ec135a50f395d75b35af5afd
fadhlu <fadhlurahman.i@gmail.com>
2020-08-27 01:55:33 +0700

    ✨ add jwt. middleware for authentication

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,12 +1,76 @@
-import { Resolver, Query, Mutation, Arg } from 'type-graphql';
-import { hash } from 'bcryptjs';
+import { isAuth } from './isAuthMiddleware';
+import { createRefreshToken, createAccessToken } from './auth';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Arg,
+  ObjectType,
+  Field,
+  Ctx,
+  UseMiddleware,
+} from 'type-graphql';
+import { hash, compare } from 'bcryptjs';
 import { User } from './entity/User';
+import { Context } from './Context';
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string;
+}
 
 @Resolver()
 export class UserResolver {
   @Query(() => String)
   hello() {
     return 'hello 🤺';
+  }
+
+  @Query(() => String)
+  @UseMiddleware(isAuth)
+  wave() {
+    return "hello i'm protected 👋";
+  }
+
+  @Query(() => String)
+  @UseMiddleware(isAuth)
+  whoami(@Ctx() { payload }: Context) {
+    console.log(payload);
+    return `youre 👉 ${payload?.userId}`;
+  }
+
+  @Query(() => [User])
+  Users() {
+    return User.find();
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg('email') email: string,
+    @Arg('password') password: string,
+    @Ctx() { res }: Context
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('no user found!');
+    }
+
+    const valid = await compare(password, user.password);
+
+    if (!valid) {
+      throw new Error('wrong password!');
+    }
+
+    // login successful
+    res.cookie('jid', createRefreshToken(user), {
+      httpOnly: true,
+    });
+
+    return {
+      accessToken: createAccessToken(user),
+    };
   }
 
   @Mutation(() => Boolean)
